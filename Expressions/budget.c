237719ef49b1336bbde35353b0eb9ff279edb4f9
Martin-Labarca <martin.labarca@ucas-edu.net>
2024-09-12 19:44:07 +0000

    budget in C

--- Expressions/budget.c
+++ Expressions/budget.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(void){
-    printf("Hello World");
+    char income[9];
+    char utilities[9];
+    char grocies[9];
+    char car[9];
+    printf("This is a budger calculator.\n How much do you make a month?\n");
+    scanf("%s", income);
+    printf("How much do your utilities cost?\n");
+    scanf("%s", utilities);
+    printf("You make %s\n", income);
+    float spend[] = (float) income - (float) utilities;
+    printf("%f" ,spend);
     return 0;
 }
