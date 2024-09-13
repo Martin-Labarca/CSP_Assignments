21a8884b2125aa128707c5fd8a4975db4495c919
Martin-Labarca <martin.labarca@ucas-edu.net>
2024-09-13 21:21:05 +0000

    added more code lines

--- Expressions/budget.c
+++ Expressions/budget.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 
 int main(void){
-    char income[9];
-    char utilities[9];
-    char grocies[9];
-    char car[9];
+    float income, rent, utilities, groceries, transportation, savings, expenses, spend;
+    float prent, putilities, pgroceries, ptransportation, psavings, pexpenses;
     printf("This is a budger calculator.\n How much do you make a month?\n");
-    scanf("%s", income);
-    printf("How much do your utilities cost?\n");
-    scanf("%s", utilities);
-    printf("You make %s\n", income);
-    float spend[] = (float) income - (float) utilities;
-    printf("%f" ,spend);
+    scanf("%f", &income);
+    printf("How much your rent cost?\n");
+    scanf("%f", &rent);
+    printf("How much your utilities cost?\n");
+    scanf("%f", &utilities);
+    printf("How much do your grocieries cost?\n");
+    scanf("%f", &groceries);
+    printf("How much your transportation cost?\n");
+    scanf("%f", &transportation);
+    savings = income * .2; 
+    expenses = rent + utilities + groceries + transportation;
+    spend = income - expenses - savings;
+    printf("You make $%.2f\n", income);
+    printf("Your expenses are $%.2f\n", expenses);
+    printf("Your savings are $%.2f\n", savings);
+    printf("Your spending money is $%.2f\n", spend);
+    prent = rent/income * 100;
+    putilities = utilities/income * 100;
+    pgroceries = groceries/income * 100;
+    ptransportation = transportation/income * 100;
+    psavings = savings/income * 100;
+    pexpenses = expenses/income * 100;
+    printf("Your rent is %d%% of your income.\n", (int) prent);
+    printf("Your utilities are %d%% of your income.\n", (int) putilities);
+    printf("Your groceries are %d%% of your income.\n", (int) pgroceries);
+    printf("Your transportation costs are %d%% of your income.\n", (int) ptransportation);
+    printf("Your savings are %d%% of your income.\n", (int) psavings);
+    printf("Your expenses are %d%% of your income.\n", (int) pexpenses);
     return 0;
 }
--- Variables/First-Program.c
+++ Variables/First-Program.c
@@ -3,7 +3,7 @@
 int main(void){
 	char name[9];
 	printf("Hello, what is your name.\n");
-	 scanf("%s", name);
+	scanf("%s", name);
 	printf("Hello %s\n", name);
 	return 0;
 }
