aae8992cad2d059c9fab872838605d2a76061dab
Martin-Labarca <martin.labarca@ucas-edu.net>
2024-09-18 00:47:17 +0000

    fixed stuff and added the decoration

--- Expressions/name_decorator.c
+++ Expressions/name_decorator.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    char name[9];
-    char tag[9];
-    char answer[3];
-
-    printf("What is your name?\n");
+    char name[50];
+    printf("Enter your name: ");
     scanf("%49s", name);
-
-    printf("Now select a tag\n");
-    scanf("%49s", tag);
-    
-    printf("Hello %s you choosed the tag: %s\n", name, tag);
+    printf("<<< %s >>>\n", name);
 
     return 0;
 }
