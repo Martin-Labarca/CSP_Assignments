e53473f53b2cd52fbf2be551c25463cc74ee3a45
Martin-Labarca <martin.labarca@ucas-edu.net>
2024-09-18 19:26:17 +0000

    Silly setences c

--- Expressions/silly_setences.c
+++ Expressions/silly_setences.c
@@ -3,16 +3,16 @@
 int main() {
     char word1[50], word2[50], word3[50];
 
-    printf("Enter the first word: ");
+    printf("Please enter a word: ");
     scanf("%49s", word1);
 
-    printf("Enter the second word: ");
+    printf("Now, enter the second word: ");
     scanf("%49s", word2);
 
-    printf("Enter the third word: ");
+    printf("Now, enter the last word: ");
     scanf("%49s", word3); 
     
-    printf("Once upon a time, a %s decided to ride a %s while eating a %s.\n", word1, word2, word3);
+    printf("Once upon a time, a %s decided to use a %s when it needed to use a %s.\n", word1, word2, word3);
 
     return 0;
 }
