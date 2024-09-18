a77dfc47fa68b78f4f82123fa94f2a967848a3aa
Martin-Labarca <martin.labarca@ucas-edu.net>
2024-09-18 19:43:22 +0000

    Equations python

---
+++ Expressions/Equations.py
@@ -0,0 +1,19 @@
+equation = 7-24/8*4+6
+equationtwo = 18/3-7+2*5
+equationthree = 6*4/12+72/8-9
+equationfour = (17-6/2)+4*3
+equationfive = -2*(1*4-2/2)+(6+2-3)
+equationsix = -1*[(3-4*7)/5]-2*24/6 
+equationseven = (3*5^2/15)-(5-2^2)
+equationeight = (1^4*2^2+3^3)-2^5/4
+equationnine = (22/2-2*5)^2+(4-6/6)^2
+
+print(equation)
+print(equationtwo)
+print(equationthree)
+print(equationfour)
+print(equationfive)
+print(equationsix)
+print(equationseven)
+print(equationeight)
+print(equationnine)
