47b67d584ad03961df17ae009af2a7e3b55ae3c8
Martin-Labarca <martin.labarca@ucas-edu.net>
2024-10-04 00:06:33 +0000

    Time Python

--- Conditionals/time.py
+++ Conditionals/time.py
@@ -1,14 +1,14 @@
 import time
 
 def main():
-    # Get the current local time and date
+
     current_time = time.localtime()
     print("Current local time and date:", time.strftime("%Y-%m-%d %H:%M:%S", current_time))
 
-    # Ask the user for the hour in military time
+
     hour = int(input("Tell me the hour in military time (0-23): "))
 
-    # Validate the input and print the appropriate greeting
+
     if hour < 0 or hour > 23:
         print("Invalid hour input.")
     elif hour < 12:
